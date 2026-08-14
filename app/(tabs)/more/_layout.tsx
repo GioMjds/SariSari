ea13abba218f44bc312d099e2e2a871eb2ebff70
GioMjds <giomjds@gmail.com>
2026-08-14 17:39:57 +0800

    feat: refactor More screen layout and navigation, add new backup timestamp formatting and tests

--- app/(tabs)/more/_layout.tsx
+++ app/(tabs)/more/_layout.tsx
@@ -1,18 +1,5 @@
-import { Redirect, Slot, usePathname } from 'expo-router';
+import { Slot } from 'expo-router';
 
 export default function MoreLayout() {
-  const pathname = usePathname();
-
-  if (!__DEV__) {
-    return (
-      <Redirect
-        href={{
-          pathname: '/unimplemented',
-          params: { route: pathname },
-        }}
-      />
-    );
-  }
-
   return <Slot />;
 }
